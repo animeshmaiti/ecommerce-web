e67f0a8f3e3044dfcedcf1bd68e391acdf601f14
Animesh Maiti <a.maiti770@gmail.com>
2023-08-07 09:52:56 +0530

    fix problem of address_result

--- client/orders.php
+++ client/orders.php
@@ -2,12 +2,16 @@
 include_once 'authguard.php';
 include_once '../shared/connection.php';
 $userid = $_SESSION['userid'];
+print_r($userid);
 
-$query = "INSERT INTO `orders` (userid, pid,seller_id) SELECT `userid`, `pid`, `seller_id` FROM `cart` WHERE `userid`=$userid";
-$result = mysqli_query($conn, $query);
+// $query = "INSERT INTO `orders` (userid, pid,seller_id) SELECT `userid`, `pid`, `seller_id` FROM `cart` WHERE `userid`=$userid";
+// $result = mysqli_query($conn, $query);
 
 $address_query = "SELECT `address_id` FROM `addresses` WHERE `userid`=$userid";
 $address_result = mysqli_query($conn, $address_query);
+if (mysqli_num_rows($address_result) == 0) {
+    header("Location: address_form.php");
+}
 $address_row = mysqli_fetch_assoc($address_result);
 $address_id = $address_row['address_id'];
 // print_r($address_id);
