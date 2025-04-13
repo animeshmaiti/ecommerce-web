a2956bb407233be548108bd8797be319b3fdaecd
animeshmaiti <a.maiti770@gmail.com>
2025-04-13 13:35:49 +0530

    handle delete product with condition

--- vendor/delete_product.php
+++ vendor/delete_product.php
@@ -1,14 +1,32 @@
 <?php
-    include_once "authguard.php";
-    include_once "../shared/connection.php";
+include_once "authguard.php";
+include_once "../shared/connection.php";
 
-    $pid=$_GET['pid'];
-    
-    $query="DELETE FROM `product` WHERE `pid`=$pid";
-    $result=mysqli_query($conn,$query);
-    if($result){
-        header("Location: view_product.php");
-    }else{
-        echo "Error: ".mysqli_error($conn);
+$pid = $_GET['pid'];
+
+// Fetch image path before deleting the product
+$query = "SELECT imgpath FROM product WHERE pid=$pid";
+$result = mysqli_query($conn, $query);
+
+if ($row = mysqli_fetch_assoc($result)) {
+    $img_path = $row['imgpath'];
+
+    // Delete the file from shared/img
+    if (file_exists($img_path)) {
+        unlink($img_path);
     }
+
+    // Delete product from database
+    $delete_query = "DELETE FROM product WHERE pid=$pid";
+    $delete_result = mysqli_query($conn, $delete_query);
+
+    if ($delete_result) {
+        header("Location: view_product.php");
+        exit();
+    } else {
+        echo "Error deleting product: " . mysqli_error($conn);
+    }
+} else {
+    echo "Product not found!";
+}
 ?>
--- vendor/upload_product.php
+++ vendor/upload_product.php
@@ -9,8 +9,10 @@
     $price=$_POST['price'];
     $details=$_POST['details'];
     // print_r($_FILES['img_path']['tmp_name']);
+    // print_r($_FILES);
     $dest_img_path="../shared/img/".$_FILES['img_path']['name'];
     // print_r($dest_img_path);
+    // exit();
     move_uploaded_file($_FILES['img_path']['tmp_name'],$dest_img_path);
 
     $query="INSERT INTO `product`(`product_name`, `price`, `details`, `imgpath`,`uploaded_by`) VALUES ('$product_name',$price,'$details','$dest_img_path','$userid')";
